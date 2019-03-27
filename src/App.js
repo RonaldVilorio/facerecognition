7a7fa6f7092f0d534e229a7790eff630ce30c9fc
RonaldVilorio <ronvilorio@gmail.com>
2019-03-27 18:05:39 -0400

    built logo component/added tilt package

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Navigation />
-        <Logo />
+        <Navigation />
+        {/* <Logo />
         <ImageLinkForm />
         <FaceRecognition /> */}
       </div>
---
+++ src/components/Navigation/Logo/Logo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Logo = () => {
+  return <div className="ma4 mt0" />;
+};
+
+export default Logo;
