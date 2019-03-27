a12feee318aac56026e637b659e8d697c5ceead3
RonaldVilorio <ronvilorio@gmail.com>
2019-03-27 19:22:09 -0400

    built rank component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Navigation/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/*<FaceRecognition /> */}
       </div>
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,14 +1,16 @@
 import React from "react";
-
+import "./ImageLinkForm.css";
 const ImageLinkForm = () => {
   return (
     <div>
       <p className="f3">{"Detect faces in pictures"}</p>
-      <div>
-        <input type="tex" className="f4 pa2 w-70 center" />
-        <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
-          Detect
-        </button>
+      <div className="center">
+        <div className="pa4 br3 shadow-5 center">
+          <input type="tex" className="f4 pa2 w-70 center" />
+          <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
+            Detect
+          </button>
+        </div>
       </div>
     </div>
   );
