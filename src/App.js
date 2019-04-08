d3ec9c9102456eab550c6a6abd503cb1ca805f7b
RonaldVilorio <ronvilorio@gmail.com>
2019-04-08 18:08:48 -0400

    built face recognition component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Navigation from "./components/Navigation/Navigation";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import Particles from "react-particles-js";
 import Logo from "./components/Logo/Logo";
+import Clarifai from "clarifai";
 
+const app = new Clarifai.App({
+  apiKey: "f563f3c6abaa4448bd4018085c74e145"
+});
 const particlesOptions = {
   particles: {
     number: {
@@ -21,27 +26,23 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      input: ""
+      input: "",
+      imageUrl: ""
     };
   }
   onInputChange = e => {
-    console.log(e.target.value);
+    this.setState({ input: e.target.value });
   };
   onButtonSubmit = () => {
-    console.log("click");
-    app.models
-      .predict(
-        "a403429f2ddf4b49b307e318f00e528b",
-        "https://samples.clarifai.com/face-det.jpg"
-      )
-      .then(
-        function(response) {
-          // do something with response
-        },
-        function(err) {
-          // there was an error
-        }
-      );
+    this.setState({ imageUrl: this.state.input });
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+      function(resp) {
+        console.log(resp.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      function(err) {
+        // there was an error
+      }
+    );
   };
 
   render() {
@@ -55,7 +56,7 @@ class App extends Component {
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        {/* <FaceRecognition /> */}
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
